c3082b8f97f2967ba00f35f7f30353fb46155bf1
PKG-7 <pavel.k.group@gmail.com>
2022-12-21 14:24:12 +0300

    auth сверстано

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useContext } from 'react';
+
+import { Context } from '../context';
+import { useRouter } from 'next/dist/client/router';
+import axios from 'axios';
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const { setUsername, setSecret } = useContext(Context);
+
+  return (
+    <div className='background'>
+      <div className='auth-container'>
+        <form onSubmit={e => e.preventDefault} className='auth-form'>
+          <div className='auth-title'>PKG Chat</div>
+
+          <div className='input-container'>
+            <input
+              type='email'
+              placeholder='Email'
+              className='text-input'
+              onChange={e => setUsername(e.target.value)}
+            />
+            <input
+              type='password'
+              placeholder='Password'
+              className='text-input'
+              onChange={e => setSecret(e.target.value)}
+            />
+
+            <button type='submit' className='submit-button'>
+              Login / Sign up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
